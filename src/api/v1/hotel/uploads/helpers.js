93c75499638866c6d4b46ee28270f84d013817b8
lash0000 <k80308392@gmail.com>
2025-03-16 12:22:47 +0800

    add: upload queue ruotes

--- src/api/v1/hotel/uploads/helpers.js
+++ src/api/v1/hotel/uploads/helpers.js
@@ -6,11 +6,13 @@ const express = require('express');
 
 const uploads_employee_records = require('./employee_records/routes');
 const uploads_rooms = require('./room_management/routes');
+const queue_uploads = require('./queues/routes');
 
 const router = express.Router();
 
 // Define it
 router.use('/employee_records', uploads_employee_records);
 router.use('/room_management', uploads_rooms);
+router.use('/queues', queue_uploads);
 
 module.exports = router;
--- src/api/v1/hotel/uploads/queues/routes.js
+++ src/api/v1/hotel/uploads/queues/routes.js
@@ -7,10 +7,10 @@ const upload = require("../../../../../../global/config/Multer");
 const uploadFields = upload.array("files[]", 10);
 
 // Define HTTP events
-router.get("/media_files/:id", uploadedById);
-router.get("/media_files", getAllFiles);
-router.post("/media_files", uploadFields, uploadFiles);
-router.delete("/media_files/:id", deleteFileById);
-router.delete("/media_files/user/:processed_by_id", deleteAllFilesByUser);
+router.get("/:id", uploadedById);
+router.get("/", getAllFiles);
+router.post("/", uploadFields, uploadFiles);
+router.delete("/:id", deleteFileById);
+router.delete("/user/:processed_by_id", deleteAllFilesByUser);
 
 module.exports = router;
